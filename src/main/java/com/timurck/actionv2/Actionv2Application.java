ae23f96aebcc9d3f1b2d4887492143baebb2f8f8
Timur-Cakmak <timur.ck.dev@gmail.com>
2025-01-04 22:01:27 +0100

    first_commit

---
+++ src/main/java/com/timurck/actionv2/Actionv2Application.java
@@ -0,0 +1,13 @@
+package com.timurck.actionv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actionv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Actionv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/timurck/actionv2/Actionv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timurck.actionv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actionv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
